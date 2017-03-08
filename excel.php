479a4dfb8cec4de8794408dd9a1202ff637aa2c8
Anthony <dkominion@yahoo.com>
2017-03-08 16:02:56 +0300

    phpexcel

---
+++ excel.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+	
+	require_once "Classes/PHPExcel.php";
+
+
+		$tmpfname = "test.xlsx";
+		$excelReader = PHPExcel_IOFactory::createReaderForFile($tmpfname);
+		$excelObj = $excelReader->load($tmpfname);
+		$worksheet = $excelObj->getSheet(0);
+		$lastRow = $worksheet->getHighestRow();
+		
+		echo "<table>";
+		for ($row = 1; $row <= $lastRow; $row++) {
+			 echo "<tr><td>";
+			 echo $worksheet->getCell('A'.$row)->getValue();
+			 echo "</td><td>";
+			 echo $worksheet->getCell('B'.$row)->getValue();
+			 echo "</td><tr>";
+		}
+		echo "</table>";	
+
+?>
+</body>
+</html>
